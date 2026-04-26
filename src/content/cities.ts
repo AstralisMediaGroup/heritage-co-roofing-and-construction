42e85ab56fc067d4ac5c888bac01c767baef2931
AstralisMediaGroup <alec@astralismediagroup.com>
2026-04-25 22:53:08 -0500

    Plan 002.0: extract founders + cities content

---
+++ src/content/cities.ts
@@ -0,0 +1,21 @@
+export const eastTexas: string[] = [
+	'Longview',
+	'Tyler',
+	'Marshall',
+	'Mt. Pleasant',
+	'Nacogdoches',
+	'Palestine',
+	'Kilgore',
+	'Henderson',
+	'Gladewater',
+	'Gilmer',
+];
+
+export const eastTexasMore = '+ 25 more cities';
+
+export const centralArkansas: string[] = [
+	'Little Rock',
+	'Conway',
+	'Russellville',
+	'Clarksville',
+];
